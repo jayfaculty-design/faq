571754773ca13403478a84351d3f6c91b6d769b6
Jay Designs <izfaculty12@gmail.com>
2024-11-18 10:57:53 +0000

    Images added

--- my-faq/src/App.jsx
+++ my-faq/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div className="top-container md:h-[300px] tablet:h-[350px] lg:h-[350px] laptop1024:h-[250px] desktop:h-[350px] laptop1024:bg-[url('./images/background-pattern-desktop.svg')] tablet:bg-[url('./images/background-pattern-desktop.svg')] lg:bg-[url('./images/background-pattern-desktop.svg')] relative">
+      <div className="top-container md:h-[300px] tablet:h-[350px] lg:h-[350px] laptop1024:h-[250px] desktop:h-[350px] laptop1024:bg-[url('./images/background-pattern-desktop.jpg')] tablet:bg-[url('./images/background-pattern-desktop.jpg')] lg:bg-[url('./images/background-pattern-desktop.jpg')] relative">
         {/* Top container content */}
       </div>
 
